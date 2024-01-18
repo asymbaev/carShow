653ec3dae470ab7b705012d092348a1940ac9fc1
Dastan <asymbaevdastan07@gmail.com>
2024-01-18 14:46:07 -0500

    completed token base

---
+++ src/main/java/com/binary/carShow/security/JwtService.java
@@ -0,0 +1,38 @@
+package com.binary.carShow.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtService {
+    static final long EXPIRATIONTIME = 86400000;
+    static final String PREFIX = "Bearer";
+    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.ES256);
+    public String getToken(String username) {
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .signWith(key)
+                .compact();
+        return token;
+    }
+    public String getAuthUser(HttpServletRequest request){
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null) {
+            String user = Jwts.parser()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token.replace(PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+            if (user != null) return user;
+
+        }
+        return null;
+    }
+}
